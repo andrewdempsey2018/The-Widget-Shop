e7e1ddc00e4642497ea0afe76e047f4477b86404
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-10-12 20:20:06 +0100

    Added fields to widget model, migrate widget

--- widget/models.py
+++ widget/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+name = models.CharField(max_length=100)
+slug = models.SlugField(max_length=100)
+description = models.TextField(max_length=200)
+features = models.TextField(max_length=200)
+mainPicBig = models.CharField(max_length=100)
+mainPicSmall = models.CharField(max_length=100)
+detailPicBig = models.CharField(max_length=100)
+detailPicSmall = models.CharField(max_length=100)
