c50171ab013244437378d5511985f8509cc36005
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-10-13 20:58:12 +0100

    added styles to index page

--- widget/models.py
+++ widget/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-name = models.CharField(max_length=100)
-slug = models.SlugField(max_length=100)
-description = models.TextField(max_length=200)
-features = models.TextField(max_length=200)
-mainPicBig = models.CharField(max_length=100)
-mainPicSmall = models.CharField(max_length=100)
-detailPicBig = models.CharField(max_length=100)
-detailPicSmall = models.CharField(max_length=100)
+class Widget(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    description = models.TextField(max_length=200)
+    features = models.TextField(max_length=200)
+    mainPicBig = models.CharField(max_length=100)
+    mainPicSmall = models.CharField(max_length=100)
+    detailPicBig = models.CharField(max_length=100)
+    detailPicSmall = models.CharField(max_length=100)
