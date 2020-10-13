33be1e97366e015faae4649928ea777ac8d9fe6d
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-10-13 21:55:47 +0100

    Card for widgets added to index

--- widgetshop/views.py
+++ widgetshop/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from widget.models import Widget
+
 def index(request):
-    return render(request, "index.html")
+    widgets = Widget.objects.all()
+    return render(request, "index.html", {'widgets': widgets})
