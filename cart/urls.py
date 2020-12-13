ac964d272e91113c412d96fd4359c9f35eee740e
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-12-13 19:37:04 +0000

    Fixed error where cart item order totals were inaccurate

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("add/<id>/", views.add, name='add'),
     path("view_cart/", views.view_cart, name='view_cart'),
     path("add_to_cart/<id>/", views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 from widget.models import Widget
 
-def add(request, id):
-    cart = request.session.get('cart', {})
-    cart['vagekey'] = get_object_or_404(Widget, pk=id)
-
-    request.session['cart'] = cart
-    return redirect(reverse('index'))
-
 def view_cart(request):
     cart = request.session.get('cart', {})
     return render(request, "cart/view_cart.html", {'cart': cart})
@@ -16,6 +9,15 @@ def view_cart(request):
 def add_to_cart(request, id):
     cart = request.session.get('cart', {})
     product = get_object_or_404(Widget, pk=id)
-    cart[id] = {'product_name': product.name, 'quantity': request.POST['quantity'], 'image': product.detailPicSmall}
+
+    if id in cart.keys():
+        val1 = int(cart[id].get('quantity'))
+        val2 = int(request.POST['quantity'])
+        val3 = val1+val2
+        cart[id]['quantity'] = val3
+    else:
+        cart[id] = {'product_name': product.name, 'quantity': request.POST['quantity'], 'image': product.detailPicSmall}
+
+    
     request.session['cart'] = cart
     return redirect('index')
